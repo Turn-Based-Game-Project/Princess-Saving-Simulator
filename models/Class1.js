d6e278febcf22dc78e8770bde30cbc5ba2e3da04
gabess3 <95196142+gabess3@users.noreply.github.com>
2023-03-08 20:59:29 -0600

    updated character models, added placeholder files to view, seeds, and utils folders.

--- models/Class1.js
+++ models/Class1.js
@@ -6,10 +6,58 @@ class Class1 extends Model {}
 Class1.init(
     {
         id:  {
-  
-          }
-      },
-      {
-          
-      },
-  )
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+        hp: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 100,
+            validate: {
+                isNumeric: true,
+            },
+        },
+        attack: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate: {
+                isNumeric: true,
+            },
+        },
+        defense: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate: {
+                isNumeric: true,
+            },
+        },
+        move_1: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        move_2: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        move_3: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        move_4: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Class1',
+    },
+)
+
+module.exports = Class1;
--- models/Class2.js
+++ models/Class2.js
@@ -6,10 +6,58 @@ class Class2 extends Model {}
 Class2.init(
     {
         id:  {
-  
-          }
-      },
-      {
-          
-      },
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+        hp: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 100,
+            validate: {
+                isNumeric: true,
+            },
+        },
+        attack: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate: {
+                isNumeric: true,
+            },
+        },
+        defense: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate: {
+                isNumeric: true,
+            },
+        },
+        move_1: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        move_2: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        move_3: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        move_4: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Class1',
+    },
 )
+
+module.exports = Class2;
